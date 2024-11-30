5096a3f9c92aa269d7ab311e4d187a47e68e750f
ch4t5ky <mr_a953@mail.ru>
2024-11-30 12:10:53 +0300

    Add Builded source

---
+++ precache-manifest.dffd867b0fd0097ad7e07890402105d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2a63cf99e00e1446d0372fbfde3f625",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8da995a2ca2d3e1b1bc2",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "e266eec883dfbc0ffb9a",
+    "url": "/static/js/2.1a240082.chunk.js"
+  },
+  {
+    "revision": "8da995a2ca2d3e1b1bc2",
+    "url": "/static/js/main.6123aca7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
